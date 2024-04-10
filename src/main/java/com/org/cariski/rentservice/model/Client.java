3f9e4c0c46ae4424bb953c32ebac6eac0d7793b4
Yehor <egorkorsun7@gmail.com>
2024-04-10 15:22:19 +0200

    Cleared

--- src/main/java/com/org/cariski/rentservice/model/Client.java
+++ src/main/java/com/org/cariski/rentservice/model/Client.java
@@ -3,10 +3,12 @@ package com.org.cariski.rentservice.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @Table(name = "Clients")
 public class Client {
 
